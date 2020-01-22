ffe9efa1d93a34b3c20ce7c0ca5d43ed81e02abc
slupekdev <slupek.dev@gmail.com>
2020-01-22 21:38:11 +0100

    feat: override existing pipeline and protractor config files

--- packages/web-ci/src/azure-devops/index.ts
+++ packages/web-ci/src/azure-devops/index.ts
@@ -1,20 +1,22 @@
-import { Rule, SchematicContext, Tree, chain, apply, url, applyTemplates, move, mergeWith } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain, apply, url, applyTemplates, move, mergeWith, MergeStrategy } from '@angular-devkit/schematics';
 import { Schema } from './schema';
+import { overwriteIfExists } from '@objectivity/angular-schematic-utils';
 
 export function azureDevOps(options: Schema): Rule {
-    return (_tree: Tree, _context: SchematicContext) => {
+    return (tree: Tree, _context: SchematicContext) => {
         return chain([
-            createVstsCiBuildYamlFile(options)
+            createVstsCiBuildYamlFile(tree, options)
         ]);
     };
 }
 
-function createVstsCiBuildYamlFile(options: Schema) {
+function createVstsCiBuildYamlFile(tree: Tree, options: Schema) {
     const templateSource = apply(url('./files'), [
         applyTemplates({
             ...options
         }),
-        move('.')
+        move('.'),
+        overwriteIfExists(tree)
     ]);
     return chain([
         mergeWith(templateSource)
