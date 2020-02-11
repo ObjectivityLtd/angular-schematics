26dd84d7f4831ff1d20741bdad6f94e22f7a5349
slupekdev <slupek.dev@gmail.com>
2020-02-11 20:33:47 +0100

    refactor: update sandbox projects to v9.0.0

--- sandboxworkspace/projects/sandbox2/src/app/app.component.spec.ts
+++ sandboxworkspace/projects/sandbox2/src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'sandbox2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app.title).toEqual('sandbox2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
+    const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('sandbox2 app is running!');
   });
 });
