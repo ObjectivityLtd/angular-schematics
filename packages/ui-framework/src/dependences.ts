602150bd090663842368dfe5bb8cfc6020625d80
slupekdev <slupek.dev@gmail.com>
2020-02-14 17:30:47 +0100

    feat: upgrade to v9

--- packages/ui-framework/src/dependences.ts
+++ packages/ui-framework/src/dependences.ts
@@ -1,9 +1,9 @@
 export const bootstrapPkg = {
     pkg: 'bootstrap',
-    version: '^4.3.0'
+    version: '^4.4.0'
 };
 
 export const materialPkg = {
     pkg: '@angular/material',
-    version: '^8.0.0'
+    version: '^9.0.0'
 };
